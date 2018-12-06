c8a3712486230d771e2544849decfb3d6717a05a
Will Elliman <william.elliman@gmail.com>
2018-12-05 20:29:26 -0500

    Main method is now in MainWindow.

--- src/MainWindow.java
+++ src/MainWindow.java
@@ -6,16 +6,20 @@ import java.io.File;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
+import javafx.application.Application;
+import javafx.stage.Stage;
+
 /**
  * This class is the main widow for the program. It holds the highest level panels and controls the exit conditions.
  * It is also responsible for passing information to the logical classes.
  * @author William Elliman
  *
  */
-public class MainWindow extends JFrame{
-	
-	private JPanel contentPane;
-	
+public class MainWindow extends Application{
+
+	private File data;
+	private int x, y;
+
 	/**
 	 * Makes a new Moving Mean Graph with an info panel
 	 * @param data : The data file that will be used to make the graph.
@@ -23,19 +27,41 @@ public class MainWindow extends JFrame{
 	 * @param y : The index in each line for the data that will be on the y axis.
 	 */
 	public MainWindow(File data, int x, int y) {
-		//window setup
-		Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
-		contentPane = new JPanel(new BorderLayout());
-		this.setContentPane(contentPane);
-		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-		this.setSize(new Dimension(screen.width/2,screen.height/2));
-		this.setTitle(data.getName());
-		
-		//create a new Graph to display
-		Graph graph = new Graph(data,x,y);
-		contentPane.add(graph, BorderLayout.CENTER);
-		System.out.println(graph.getHeight());
+		this.data = data;
+		this.x = x;
+		this.y = y;
 	}
-	
-	
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		//this is the entry point for the application
+
+		primaryStage.setTitle(data.getName());
+	}
+
+	public static void main(String[] args) {
+
+		File data = null;
+		int x = -1,y = -1;
+
+		//check args for the values needed.
+		for(int i = 0; i < args.length; i++) {
+			if(!args[i].startsWith("-")) {
+				continue;
+			} else if(args[i].equalsIgnoreCase("-d")){
+				//the -d command is for a data file that follows
+				data = new File(args[i+1]);
+			} else if(args[i].equalsIgnoreCase("--x")) {
+				x = Integer.parseInt(args[i+1]);
+			} else if(args[i].equalsIgnoreCase("--y")) {
+				y = Integer.parseInt(args[i+1]);
+			}
+		}
+
+		if(data != null && x != -1 && y != -1) {
+			MainWindow window = new MainWindow(data,x,y);
+			window.launch(args);
+		}
+
+	}
 }
