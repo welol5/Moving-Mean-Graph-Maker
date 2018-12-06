234ecc37e8268e0df03e2aa466a67434f82baaba
Will Elliman <william.elliman@gmail.com>
2018-12-05 20:56:50 -0500

    Added some testing stuff for learning javafx.

--- src/MainWindow.java
+++ src/MainWindow.java
@@ -7,6 +7,11 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 /**
@@ -23,7 +28,23 @@ public class MainWindow extends Application{
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		//this is the entry point for the application
+		primaryStage.setTitle("Data");
+		primaryStage.show();
 		
+		//make a scene
+		Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
+		Group root = new Group();
+		Canvas canvas = new Canvas(screen.width/2,screen.height/2);
+		GraphicsContext gc = canvas.getGraphicsContext2D();
+		fillGraph(gc);
+		root.getChildren().add(canvas);
+		primaryStage.setScene(new Scene(root));
+		primaryStage.show();
+	}
+	
+	public void fillGraph(GraphicsContext gc) {
+		gc.setFill(Color.BLUEVIOLET);
+		gc.fillText("Hello", 50, 50);
 	}
 
 	public static void main(String[] args) {
